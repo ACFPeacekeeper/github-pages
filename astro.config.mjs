77ceb97e4382d89a8def921cc8c20f544832a02d
ACFHarbinger <afonso.fernandes100@gmail.com>
2026-08-08 17:28:45 +0100

    refactor(website): move Astro config to root and src/astro/src to src

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   srcDir: './src',
-  outDir: '../../public/astro-island',
-  publicDir: './public',
+  outDir: './public/astro-island',
+  publicDir: './astro-public',
   base: '/github-pages/astro-island',
   vite: {
     css: {
